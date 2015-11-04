57a20273b0743986024dc18955a512192e9a48c7
Jason Williams <jase.williams@gmail.com>
2015-11-04 01:53:21 +0000

    error

--- hashweb.js
+++ hashweb.js
@@ -134,7 +134,7 @@ module.exports = {
 
     karmaHelp: function(context, user) {
       context.channel.send_reply(context.sender, "Every user has Karma points, these can be gained by helping people or having someone give you karma with !giveKarma [user], you can check other user's karma with !karmaLevel [user], or your own with just !karma");
-    }
+    },
 
     karma: function(context, help) {
       if (typeof help !== "undefined" && help === "help") {
